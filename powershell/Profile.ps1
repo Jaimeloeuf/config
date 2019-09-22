d343c00ce1096c47b12a67bd88f1d398a15420a2
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-09-22 22:03:29 +0800

    Updated remix function in Powershell profile:     Wrote in a switching function, where I can specify which shell to run     Local Remix-IDE with.

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -18,9 +18,23 @@ function gp { git push $args }
 function back-dir { cd .. }
 Set-Alias -Name .. -Value back-dir
 
-<# Scripts for Remix-IDE #>
-# Abbrev. to start Remix-IDE
-function remix { start remix-ide }
+<# Function for running local Remix-IDE
+    Takes in an arguement of either "p" or "c", specifying powershell or cmd to be used for running remix
+    If no inputs are given, the default shell, CMD, will be used to run remix
+#>
+function remix {
+    if ($args -eq "p") {
+        write-host("Running remix with Powershell")
+        start powershell { remix-ide } # Run with Powershell
+    } elseif ($args -eq "c") {
+        write-host("Running remix with CMD")
+        start remix-ide # Run with CMD
+    } else {
+        write-host("Invalid shell specified to run remix in.")
+        write-host("Default shell: CMD will be used to run remix.")
+        start remix-ide # Run with CMD
+    }
+}
 
 # Abbrevation to start "powershell" in new window
 function psh { Invoke-Item $PSHOME\powershell.exe }
